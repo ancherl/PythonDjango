7e9a4a5afff43464e85ac33568fdaff511dd8943
daixin <AncherlDai@163.com>
2020-08-01 15:57:53 +0800

    Model Relationship

--- polls/models.py
+++ polls/models.py
@@ -8,3 +8,11 @@ class Questions(models.Model):
     # 魔法函数: 当print对象实例时， 自动触发此方法
     def __str__(self):
         return '(question_text=' + self.question_text + ', pub_date=' + str(self.pub_date) + ')'
+
+class Choices(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '(choice_text=' + self.choice_text + ', votes=' + str(self.votes) + ')'
